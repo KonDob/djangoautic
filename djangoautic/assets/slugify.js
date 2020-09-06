08912bdb32301d9bfac09c7a47a4186152a36fc3
KonDob <konstantin.dobro@gmail.com>
2020-09-06 14:05:43 +0300

    Added whole project

---
+++ djangoautic/assets/slugify.js
@@ -0,0 +1,14 @@
+const titleInput = document.querySelector('input[name=title]')
+const sluginput = document.querySelector('input[name=slug]')
+
+const slugify = (val) =>{
+    return val.toString().toLowerCase().trim()
+    .replace(/&/g, '-and-') //replace & '-and-'
+    .replace(/[\s\W-]+/g, '-') //replace spaces, non word chars and dashes with a single dash
+
+};
+
+
+titleInput.addEventListener('keyup',(e)=>{
+    sluginput.setAttribute('value', slugify(titleInput.value));
+})
