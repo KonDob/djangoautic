e73b7e521d5d2e537aeef6b914f1954ac6fd9b41
KonDob <konstantin.dobro@gmail.com>
2020-10-27 18:36:53 +0200

    Add comments functional

--- djangoautic/articles/forms.py
+++ djangoautic/articles/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from . import models
+from django.utils.translation import gettext_lazy as _
+
 
 class CreateArticle(forms.ModelForm):
     class Meta:
         model = models.Article
         fields = ['title', 'body', 'slug','thumb']
 
+
 class CreateComment(forms.ModelForm):
     class Meta:
         model = models.Comment
         fields = ['body']
+        labels = {
+            'body': _('Place your comment')
+        }
--- djangoautic/articles/urls.py
+++ djangoautic/articles/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, re_path
+from django.urls import re_path
+from django.conf.urls import include, url
 from . import views
 
-app_name='articles'
+app_name = 'articles'
 
 urlpatterns = [
     re_path(r'^$', views.article_list, name="list"),
     re_path(r'^create/$', views.article_create, name="create"),
+    re_path(r'^/create_comment/$', views.create_comment,
+            name='create_comment'),
     re_path(r'^(?P<slug>[\w]+)/$', views.article_detail, name='detail'),
-    re_path(r'create_comment$', views.create_comment, name='create_comment'),
 ]
